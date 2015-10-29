9b824eebf23f0a6c48369e1d57192e1d8f416443
Ettore Landini <ettore.landini@outlook.it>
2015-10-29 18:58:51 +0100

    New test interface and new CMDREC
    The new interface can ask the hardware to return a set of parameters.
    The requests and the responses are managed using CMD and QtCMDREC
    classes.

--- epz/epz.py
+++ epz/epz.py
@@ -88,12 +88,13 @@ class CMD(object):
 
 
 class SkelCMDREC(object):
-    def __init__(self, environment,device = None,tag='RES'):
+    def __init__(self, environment,device = None,tag='RES',oneShot = False):
         self.context = environment.context
         self.subport = environment.subport
         self.epserver = environment.epserver
         self.tag = tag
         self.listen = True
+        self.oneShot = oneShot
         if device is None:
             self.device = environment.device
         else:
@@ -113,6 +114,12 @@ class SkelCMDREC(object):
         
     def run(self):
         self.setZmq()
+
+        if self.oneShot:
+            body = self.socket.recv_string()
+            resp = body.strip(self.head)
+            self.react(resp)
+            return True
         
         while self.listen:
             body = self.socket.recv_string()
@@ -231,10 +238,10 @@ class DATA(Skeldata, threading.Thread):
 
 class CMDREC(SkelCMDREC,threading.Thread):
     
-    def __init__(self,environment,device = None,tag = 'RES'):
+    def __init__(self,environment,device = None,tag = 'RES',oneShot = False):
         
         threading.Thread.__init__(self)
-        SkelCMDREC.__init__(self, environment,device,tag)
+        SkelCMDREC.__init__(self, environment,device,tag,oneShot)
 
 
 try:
@@ -275,10 +282,10 @@ try:
         
         respReceived = pyqtSignal(str,name='respReceived')
         
-        def __init__(self,environment,device = None,tag = 'RES'):
+        def __init__(self,environment,device = None,tag = 'RES',oneShot = False):
             
             QThread.__init__(self)
-            CMDREC.__init__(self, environment,device,tag)
+            SkelCMDREC.__init__(self, environment,device,tag,oneShot)
             
         
         def react(self,resp):
