14e87220b9939b79bcb9bf92a8a5d0f944d7fd14
elandini <ettore.landini@outlook.it>
2018-03-30 18:52:25 +0200

    0.2.11 - Modifications to LaDataRec and SkelLaDataRec

--- epz/core/laDataRec.py
+++ epz/core/laDataRec.py
@@ -16,7 +16,7 @@ class LaDataRec(SkelLaDataRec, Thread):
         self.setCallback(callback,1)
 
 
-    def setValueCallback(self,callback):
+    def setNotifyCallback(self,callback):
         self.setCallback(callback,2)
 
 
@@ -25,26 +25,14 @@ class LaDataRec(SkelLaDataRec, Thread):
             self._callData = callback
             self.notify = True
         elif type== 2:
-            self._callValue = callback
-        elif type == 3:
-            self._callSave = callback
-        elif type == 4:
-            self._callOverload = callback
+            self._callNotify = callback
 
 
-    def _actondata(self,v):  #notify about a data pack received
+    def _actOnData(self,v):  #notify about a data pack received
         if self._callData is not None:
             self._callData(v)
 
 
-    def _actOnValue(self,v):  #notify about a value received
-        if self._callValue is not None:
-            self._callValue(v)
-
-    def _switchState(self,state):  #act on SAVE state change
-        if self._callSave is not None:
-            self._callSave(state)
-
-    def _switchLoad(self,state):  #act on OVERLOAD state change
-        if self._callOverload is not None:
-            self._callOverload(state)
+    def _notification(self,v):  #notify about a value received
+        if self._callNotify is not None:
+            self._callNotify(v)
--- epz/core/skelLaDataRec.py
+++ epz/core/skelLaDataRec.py
@@ -15,13 +15,15 @@ class SkelLaDataRec(epzobject):
         self.listen = True #Set to false to finish receiving
         self.decimate = 1 #if >1 decimate the received data array before sending for management
         self.chunk = 10000 #Size of data to take in memory before transferring to the program
-        self.tickLength = 1000 #Number of samples after which notify the last value
-        self._tick = self.tickLength
         self._data = []  #DataList
         self._notify = False #Set to true to notify and transfer the data array
+        self._oldNotify = False
+        self._acting = False
+        self._oldActing = False
+        self._paused = False
 
+        self._callNotify = None
         self._callData = None
-        self._callValue = None
 
         self._socket = self.context.socket(zmq.SUB)
         self._head = "{0}:{1}:".format(self.device, self.tag)
@@ -45,9 +47,41 @@ class SkelLaDataRec(epzobject):
         self._notify = val
 
 
+    @property
+    def act(self):
+
+        return self._acting
+
+
+    @act.setter
+    def act(self,value):
+
+        self._acting = value
+
     def stop(self):
         self.listen = False
         self._stopit.send(0,0,0)
+
+
+    def pause(self):
+
+        self._paused = not self._paused
+
+        if self._paused:
+            if self.notify:
+                self._oldNotify = self._notify
+                self.notify = not self._paused
+            if self.act:
+                self._oldActing = self._acting
+                self.act = not self._paused
+        else:
+            if self._oldNotify:
+                self._oldNotify = self.notify
+                self.notify = not self._paused
+            if self._oldActing:
+                self._oldActing = self.act
+                self.act = not self._paused
+
 
 
     def run(self):
@@ -55,11 +89,8 @@ class SkelLaDataRec(epzobject):
         while self.listen:
             body = self._socket.recv_string()
             data = [float(x) for x in body[len(self._head):].split(':')] #NB: message is expected to be a list of numbers
-
-            self._tick -= 1
-            if self._tick == 0:
-                self._actOnValue(data)
-                self._tick = self.tickLength
+            if self.act:
+                self._actOnData(data)
 
             if self.notify:
                 if len(self.data) == 0:
@@ -67,12 +98,13 @@ class SkelLaDataRec(epzobject):
                 for i in range(len(data)):
                     self.data[i].append(data[i])
                 if len(self.data[0]) >= self.chunk:
-                    self._actondata(self.data)
+                    self._notification(self.data)
                     self.data = []
 
 
-    def _actondata(self,v):  #notify about a data pack received
+    def _notification(self,v):  #notify about a data pack received
         pass
 
-    def _actOnValue(self,v):  #notify about a value received
+
+    def _actOnData(self,v):
         pass
