998fdf26e7199177947bf723af9ed11ee89af9f4
elandini <ettore.landini@outlook.it>
2018-08-29 18:27:41 +0200

    2.0.0 - Major release 2.0 errata corrige

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2.0',
     packages=['epz','epz.core','epz.epsilonpi','epz.tools'],
     url='https://github.com/massimovassalli/epz',
-    download_url='https://github.com/massimovassalli/epz/releases/tag/v1.2',
+    download_url='https://github.com/massimovassalli/epz/releases/tag/v2.0',
     keywords=['zmq','pyzmq','zeromq'],
     license='',
     author='Massimo Vassalli',
